24621855bc691d2982e1630d29e5d040b0071bd7
Justadudeinspace <justadudeinspace4244@hotmail.com>
2025-07-30 18:06:37 -0400

    Fix: add missing WebAppInterface.java to match MainActivity

--- app/src/main/java/com/termuxbackground/WebAppInterface.java
+++ app/src/main/java/com/termuxbackground/WebAppInterface.java
@@ -1,30 +1,39 @@
 package com.termuxbackground;
 
 import android.content.Context;
+import android.util.Base64;
 import android.webkit.JavascriptInterface;
-import android.widget.Toast;
-
-import java.io.File;
+import java.io.*;
 
 public class WebAppInterface {
-    Context mContext;
+    Context context;
 
-    WebAppInterface(Context c) {
-        mContext = c;
+    public WebAppInterface(Context c) {
+        context = c;
     }
 
     @JavascriptInterface
-    public void showToast(String message) {
-        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
-    }
-
-    @JavascriptInterface
-    public void runInstallerScript() {
+    public void applyImage(String base64Image, String opacity, String scale, boolean blur) {
         try {
-            Process p = Runtime.getRuntime().exec(new String[]{
-                "sh", mContext.getFilesDir().getAbsolutePath() + "/install.sh"
-            });
-            p.waitFor();
+            File termuxDir = new File("/data/data/com.termux/files/home/.termux");
+            if (!termuxDir.exists()) termuxDir.mkdirs();
+
+            byte[] decodedBytes = Base64.decode(base64Image.split(",")[1], Base64.DEFAULT);
+            FileOutputStream imageOut = new FileOutputStream(new File(termuxDir, "background.png"));
+            imageOut.write(decodedBytes);
+            imageOut.close();
+
+            File propFile = new File(termuxDir, "termux.properties");
+            FileWriter writer = new FileWriter(propFile, false);
+            writer.write("background-opacity=" + (Integer.parseInt(opacity) / 100.0) + "\n");
+            writer.write("background-style=" + scale + "\n");
+            if (blur)
+                writer.write("background-blur=true\n");
+            writer.close();
+
+            Runtime.getRuntime().exec(new String[]{"am", "broadcast", "--user", "0",
+                    "-a", "com.termux.api.action.RUN_COMMAND",
+                    "--es", "com.termux.api.extra.COMMAND", "termux-reload-settings"});
         } catch (Exception e) {
             e.printStackTrace();
         }
