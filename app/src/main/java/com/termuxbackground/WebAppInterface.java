1e15e8f23921a2d8ed4017855557a4cd8bf898a6
Justadudeinspace <justadudeinspace4244@hotmail.com>
2025-07-30 21:36:03 -0400

    ✨ Add animation preview, blur/opacity controls, and termux.properties sync via UI bridge

--- app/src/main/java/com/termuxbackground/WebAppInterface.java
+++ app/src/main/java/com/termuxbackground/WebAppInterface.java
@@ -1,42 +1,34 @@
 package com.termuxbackground;
 
 import android.content.Context;
-import android.util.Base64;
 import android.webkit.JavascriptInterface;
-import java.io.*;
 
 public class WebAppInterface {
-    Context context;
+    private Context mContext;
+    private String animation = "none";
+    private String blur = "false";
+    private String opacity = "1.0";
 
-    public WebAppInterface(Context c) {
-        context = c;
+    public WebAppInterface(Context context) {
+        mContext = context;
     }
 
     @JavascriptInterface
-    public void applyImage(String base64Image, String opacity, String scale, boolean blur) {
-        try {
-            File termuxDir = new File("/data/data/com.termux/files/home/.termux");
-            if (!termuxDir.exists()) termuxDir.mkdirs();
+    public void setSettings(String anim, String blurVal, String opac) {
+        this.animation = anim;
+        this.blur = blurVal;
+        this.opacity = opac;
+    }
 
-            byte[] decodedBytes = Base64.decode(base64Image.split(",")[1], Base64.DEFAULT);
-            FileOutputStream imageOut = new FileOutputStream(new File(termuxDir, "background.png"));
-            imageOut.write(decodedBytes);
-            imageOut.close();
+    public String getAnimation() {
+        return animation;
+    }
 
-            File propFile = new File(termuxDir, "termux.properties");
-            FileWriter writer = new FileWriter(propFile, false);
-            writer.write("background-opacity=" + (Integer.parseInt(opacity) / 100.0) + "\n");
-            writer.write("background-style=" + scale + "\n");
-            if (blur)
-                writer.write("background-blur=true\n");
-            writer.close();
+    public String getBlur() {
+        return blur;
+    }
 
-            Runtime.getRuntime().exec(new String[]{"am", "broadcast", "--user", "0",
-                    "-a", "com.termux.api.action.RUN_COMMAND",
-                    "--es", "com.termux.api.extra.COMMAND", "termux-reload-settings"});
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public String getOpacity() {
+        return opacity;
     }
 }
-
